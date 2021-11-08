59cdc98b21b9c9d8cd340f6ee8518fc9eedcf937
Ryosuke Inoue <shiropenko@gmail.com>
2021-11-08 23:31:23 +0900

    Solver 05 Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,67 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+  // Get pressed key
+  @KBD
+  D=M
+  
+  // if (Memory[KBD]) > 0 then goto FILL else goto CLEAR
+  @FILL
+  D;JGT
+  @CLEAR
+  0;JMP
+
+(FILL)
+  // Initialize
+  @i
+  M=0
+  
+(FILL_LOOP)
+  // Fill Memory[SCREEN + i]
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  // DEC(-1) is equal to BIN(1111111111111111)
+  M=-1
+  
+  // Increment i
+  @i
+  MD=M+1
+
+  // if (i - 8192 < 0) then goto FILL_LOOP
+  // 512 * 256 / 16 = 8192
+  @8192
+  D=D-A
+  @FILL_LOOP
+  D;JLT
+
+  @LOOP
+  0;JMP
+
+// Similar to FILL
+(CLEAR)
+  @i
+  M=0
+  
+(CLEAR_LOOP)
+  @i
+  D=M
+  @SCREEN
+  A=D+A
+  M=0
+  
+  @i
+  MD=M+1
+
+  @8192
+  D=D-A
+
+  @CLEAR_LOOP
+  D;JLT
+
+  @LOOP
+  0;JMP
