92da0b253d4158813406bc907e99ec74ceed9536
Ryosuke Inoue <shiropenko@gmail.com>
2021-12-10 02:59:47 +0900

    Add the function of file output

--- 06/hack_assembler/src/main.rs
+++ 06/hack_assembler/src/main.rs
@@ -1,25 +1,54 @@
-mod parser;
+use std::fs::File;
+use std::io::{Write, BufWriter};
 mod code;
-use crate::parser::*;
+mod parser;
 use crate::code::*;
+use crate::parser::*;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filepath = &args[1];
-    println!("filepath:{}", filepath);
+    // println!("filepath:{}", filepath);
 
     let mut parser = Parser::new(filepath);
-    println!("{:#?}", parser);
-    println!("------------------");
+    // println!("{:#?}", parser);
+    // println!("------------------");
+
+    let mut writer = BufWriter::new(File::create("./aaa.hack").unwrap());
 
     loop {
         if parser.has_more_commands() == false {
             break;
         }
         parser = parser.advance();
-        println!("{:#?}", parser);
-        println!("------------------");
-    }
+        let cmd_type = parser.command_type().unwrap();
+        let output_bin;
 
+        if cmd_type == CommandType::ACommand {
+            let symbol: i32 = parser.symbol().unwrap().parse().unwrap();
+            output_bin = format!("0{:b>015}\n", symbol);
+        } else if cmd_type == CommandType::CCommand {
+            let dest = Code::dest(parser.dest().unwrap());
+            let comp = Code::comp(parser.comp().unwrap());
+            let jump = Code::jump(parser.jump().unwrap());
+            output_bin = format!(
+                "111{comp}{dest}{jump}\n",
+                comp = comp,
+                dest = dest,
+                jump = jump
+            );
+        } else if cmd_type == CommandType::LCommand {
+            // TODO
+            output_bin = "LCommand\n".to_string();
+            // let symbol = parser.symbol().unwrap();
+        } else {
+            output_bin = "\n".to_string();
+        }
+        writer.write_all(output_bin.as_bytes()).unwrap();
+        // println!("{:#?}", parser);
+        // println!("{}", output_bin);
+        // println!("------------------");
+    }
 }
+
